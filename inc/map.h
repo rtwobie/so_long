2c5d008f3842c5e87c1f188a039f97d1f50a2ea6
R2B <robeats.hale@gmail.com>
2025-03-29 01:46:41 +0100

    refactor: functions to map.h

--- inc/map.h
+++ inc/map.h
@@ -1,21 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   window.h                                           :+:      :+:    :+:   */
+/*   map.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/27 02:22:00 by rha-le            #+#    #+#             */
-/*   Updated: 2025/03/27 02:23:15 by rha-le           ###   ########.fr       */
+/*   Created: 2025/03/28 14:04:23 by rha-le            #+#    #+#             */
+/*   Updated: 2025/03/28 18:50:55 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef WINDOW_H
-# define WINDOW_H
+#ifndef MAP_H
+# define MAP_H
 
 # include "structs.h"
+# include <unistd.h>
 
-int		close_window(t_app *game);
-void	init_game(t_app *game, char *mapname);
+# ifndef BYTES_RD
+#  define BYTES_RD 1024
+# endif
 
-#endif // !WINDOW_H
+int	read_map(char *filename, t_map *map);
+int	get_dimensions(char *s, int *x, int *y);
+
+#endif //!MAP_H
