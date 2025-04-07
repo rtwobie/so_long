e12768045a3a4fb13ad2edeacdd3b51f5c3aa279
R2B <robeats.hale@gmail.com>
2025-04-07 23:27:19 +0200

    feat: map struct holds all initial object positions now

--- inc/structs.h
+++ inc/structs.h
@@ -6,7 +6,7 @@
 /*   By: rha-le <rha-le@student.42berlin.de>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/26 20:38:55 by rha-le            #+#    #+#             */
-/*   Updated: 2025/03/31 18:03:42 by rha-le           ###   ########.fr       */
+/*   Updated: 2025/04/07 19:55:13 by rha-le           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,27 +15,25 @@
 
 typedef struct s_entity
 {
-	void	*tex;
-	int		w;
-	int		h;
 	int		x;
 	int		y;
 }	t_entity;
 
 typedef struct s_map
 {
-	char	**lvl;
-	int		w;
-	int		h;
-	int		start_x;
-	int		start_y;
+	char		**lvl;
+	int			w;
+	int			h;
+	int			coin_count;
+	t_entity	*coin;
+	t_entity	exit;
+	t_entity	player;
 }	t_map;
 
 typedef struct s_app
 {
 	void		*app;
 	void		*win;
-	t_entity	*player;
 }	t_app;
 
 #endif // !STRUCTS_H
